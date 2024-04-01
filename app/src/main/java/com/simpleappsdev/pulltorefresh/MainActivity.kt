2d11aa1a37e304d115e120604bb75d2ed2086333
valentyn <valentinberkatyuk@gmail.com>
2024-04-01 06:37:45 +0300

    small updates

--- app/src/main/java/com/simpleappsdev/pulltorefresh/MainActivity.kt
+++ app/src/main/java/com/simpleappsdev/pulltorefresh/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -21,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.simpleappsdev.pulltorefresh.ui.theme.PullToRefreshTheme
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -28,14 +30,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val items = (1..10).map { "item $it" }
+
+            var data by remember {
+                mutableStateOf(items)
+            }
             PullToRefreshTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val items = (1..100).map { "item $it" }
-                    
                     var isRefreshing by rememberSaveable {
                         mutableStateOf(false)
                     }
@@ -45,7 +50,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                     ){
                         pullToRefreshWithListOfItems(
-                            items = items,
+                            items = data,
                             content = { itemTitle ->
                                  Text(
                                      text = itemTitle,
@@ -56,7 +61,8 @@ class MainActivity : ComponentActivity() {
                             onRefresh = {
                                 scope.launch {
                                     isRefreshing = true
-                                    delay(3000L)
+                                    data = (10..20).map { "item $it" }
+                                    delay(500)
                                     isRefreshing = false
                                 }
                             }
